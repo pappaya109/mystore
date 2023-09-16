eaddded474292914783ebae9e17452c9bec328b9
SSJ <sonsj33@gmail.com>
2023-09-16 19:14:24 +0900

    노드 api 연결

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request')
+// const fetch = require('node-fetch')
 const app = express();
 var cors = require('cors');
 app.use(express.json());
@@ -7,6 +9,19 @@ app.use(cors());
 
 
 
+
+app.get('/api', (req, res)=>{
+  const url = 'https://openapi.naver.com/v1/search/shop.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=sim'
+  const options = {
+    url: url,
+    headers: {'X-Naver-Client-Id':'GFIJm2wRn8RgcdUdZhAw', 'X-Naver-Client-Secret': '2RS1O1yUiB'}
+  };
+  request.get(options,(errorr, response, body)=>{
+    console.log('뭐보내냐',body, response)
+    res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+    res.end(body)
+  })
+})
 
 
 
--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import Login from './components/Login'
 import Products from './components/Products'
 import Details from './components/Details'
 import { Reset } from 'styled-reset'
-
+import axios from 'axios';
 function App() {
+
+  axios.get('/api')
+  .then((result)=>{return(console.log(result))})
+
   return (
     <div>
       <Reset/>
